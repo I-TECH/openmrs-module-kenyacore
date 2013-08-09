2945c771d1288bedf92beaa88cce95640a4901ad
Rowan Seymour <rowanseymour@gmail.com>
2013-08-09 10:24:26 +0200

    Initial commit with very limited metadata functionality copied from kenyaemr

---
+++ README.md
@@ -0,0 +1,4 @@
+KenyaCORE OpenMRS Module
+===========================
+
+TODO
---
+++ api/src/main/java/org/openmrs/module/kenyacore/CoreActivator.java
@@ -0,0 +1,69 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyacore;
+
+import org.apache.commons.logging.Log; 
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.ModuleActivator;
+
+/**
+ * This class contains the logic that is run every time this module is either started or stopped.
+ */
+public class CoreActivator implements ModuleActivator {
+	
+	protected static final Log log = LogFactory.getLog(CoreActivator.class);
+		
+	/**
+	 * @see ModuleActivator#willRefreshContext()
+	 */
+	public void willRefreshContext() {
+		log.info("Refreshing KenyaCORE...");
+	}
+	
+	/**
+	 * @see ModuleActivator#contextRefreshed()
+	 */
+	public void contextRefreshed() {
+		log.info("KenyaCORE refreshed");
+	}
+	
+	/**
+	 * @see ModuleActivator#willStart()
+	 */
+	public void willStart() {
+		log.info("Starting KenyaCORE...");
+	}
+	
+	/**
+	 * @see ModuleActivator#started()
+	 */
+	public void started() {
+		log.info("KenyaCORE started");
+	}
+	
+	/**
+	 * @see ModuleActivator#willStop()
+	 */
+	public void willStop() {
+		log.info("Stopping KenyaCORE...");
+	}
+	
+	/**
+	 * @see ModuleActivator#stopped()
+	 */
+	public void stopped() {
+		log.info("KenyaCORE stopped");
+	}
+}
