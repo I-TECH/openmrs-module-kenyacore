265284a6cefd34d07eeeb5a2412a572037df2167
Rowan Seymour <rowanseymour@gmail.com>
2013-08-09 16:09:06 +0200

    Added forms, programs and identifier functionality from kenyaemr

---
+++ api/src/main/java/org/openmrs/module/kenyacore/CoreUtils.java
@@ -0,0 +1,69 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyacore;
+
+import org.openmrs.calculation.ConfigurableCalculation;
+import org.openmrs.calculation.patient.PatientCalculation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Utility functions
+ */
+public class CoreUtils {
+
+	/**
+	 * Merges multiple collections into a list with natural ordering of elements
+	 * @param collections the collections
+	 * @param <T> the element type
+	 * @return the merged list
+	 */
+	public static <T extends Comparable> List<T> merge(Collection<T>... collections) {
+		Set<T> merged = new TreeSet<T>();
+
+		for (Collection<T> list : collections) {
+			for (T element : list) {
+				merged.add(element);
+			}
+		}
+
+		return new ArrayList(merged);
+	}
+
+	/**
+	 * Instantiates and configures a patient calculation
+	 * @param clazz the calculation class
+	 * @param configuration the configuration
+	 * @return the calculation instance
+	 */
+	public static PatientCalculation instantiateCalculation(Class<? extends PatientCalculation> clazz, String configuration) {
+		try {
+			PatientCalculation calc = clazz.newInstance();
+
+			if (configuration != null && clazz.isAssignableFrom(ConfigurableCalculation.class)) {
+				((ConfigurableCalculation) calc).setConfiguration(configuration);
+			}
+
+			return calc;
+		}
+		catch (Exception ex) {
+			return null;
+		}
+	}
+}
---
+++ api/src/test/java/org/openmrs/module/kenyacore/CoreUtilsTest.java
@@ -0,0 +1,40 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyacore;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.contains;
+
+/**
+ * Tests for {@link CoreUtils}
+ */
+public class CoreUtilsTest {
+
+	/**
+	 * @see CoreUtils#merge(java.util.Collection[])
+	 */
+	@Test
+	public void merge_shouldMergeByNaturalOrder() {
+		List<Integer> list1 = Arrays.asList(2, 5, 9);
+		List<Integer> list2 = Arrays.asList(1, 3, 10);
+
+		Assert.assertThat(CoreUtils.merge(list1, list2), contains(1, 2, 3, 5, 9, 10));
+	}
+}
